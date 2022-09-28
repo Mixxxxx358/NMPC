91fe71de5df714582dd0587eb33f3c92e5955698
Mixxx358 <janhh9@hotmail.com>
2022-09-28 17:05:10 +0200

    add correct fitting

--- Systems.py
+++ Systems.py
@@ -36,3 +36,22 @@ class FullStateDuffingOscillator(deepSI.System_deriv):
 
     def h(self,x,u):
         return x
+
+class UnbalancedDisc(deepSI.System_deriv):
+    def __init__(self):
+        super(UnbalancedDisc, self).__init__(nx=2, dt=0.1)
+        self.g = 9.80155078791343
+        self.J = 0.000244210523960356
+        self.Km = 10.5081817407479
+        self.I = 0.0410772235841364
+        self.M = 0.0761844495320390
+        self.tau = 0.397973147009910
+
+    def deriv(self,x,u):
+        z1,z2 = x
+        dz1 = -self.M*self.g*self.I/self.J*np.sin(z2) - 1/self.tau*z1 + self.Km/self.tau*u
+        dz2 = z1
+        return [dz1,dz2]
+
+    def h(self,x,u):
+        return x
